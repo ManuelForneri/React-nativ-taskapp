bc88503925cc2aba4daefd78594800a8393ddbd0
ManuelForneri <manuelforneri@hotmail.com>
2024-01-31 20:57:39 -0300

    Se añadieron fuentes

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import { colors } from "./src/global/colors";
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors,
+    backgroundColor: colors.fuchsiablue50,
     alignItems: "center",
     justifyContent: "center",
   },
