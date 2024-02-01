ec4234e3a92bffcbda14f3a7de13eaddb9178082
ManuelForneri <manuelforneri@hotmail.com>
2024-01-31 21:00:01 -0300

    Actualizacion de app

--- App.jsx
+++ App.jsx
@@ -1,14 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { colors } from "./src/global/colors";
-export default function App() {
+import { useFonts } from "expo-font";
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
