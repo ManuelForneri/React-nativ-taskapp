ded1935ae4a60f157c3596d095e63a95be13c9b8
Manuel Forneri <77463820+ManuelForneri@users.noreply.github.com>
2024-02-20 12:36:41 -0300

    Delete src/firebase directory

--- src/firebase/db.jsx
+++
@@ -1,4 +0,0 @@
-export const base_url =
-  "https://proyecto-react-nativ-default-rtdb.firebaseio.com/";
-export const api_key = "AIzaSyCY78c1CETaIp7bfH3F4EK8wQhxUirlmug";
-export const auth_base_url = "https://identitytoolkit.googleapis.com/v1/";
